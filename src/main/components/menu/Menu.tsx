1016338aed55468de96841f6b3b87cd0abe1b42a
Maximilian Oberholtzer <max.oberholtzer@yahoo.com>
2023-05-02 16:44:49 -0400

    Adjusted transition timing

--- src/main/components/menu/Menu.tsx
+++ src/main/components/menu/Menu.tsx
@@ -127,7 +127,7 @@ const Menu = (props: MenuProps) => {
     setTimeout(() => {
       setGameActive(true);
       MenuContainerElement?.classList.remove("fade-out-left");
-    }, 350);
+    }, 400);
   };
 
   return (
